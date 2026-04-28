4d6b86f997996caf6153fef661ac596d1457dd73
Bruno <brunoviniciuspaese@gmail.com>
2026-04-28 20:22:11 -0300

    feat(endpoints): adiciona novos atributos para atendente

--- PayflowApi/Dtos/Cashier/CreateCashierDto.cs
+++ PayflowApi/Dtos/Cashier/CreateCashierDto.cs
@@ -2,7 +2,11 @@
 {
     public class CreateCashierDto
     {
+        public string Cpf { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
         public IEnumerable<int> Rating { get; set; } = [];
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
